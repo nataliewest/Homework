1d7766cd3a1bc9757a2c63a433df9220de136f9b
nataliewest <natalie.west@serenaandlily.com>
2016-04-05 21:32:56 -0700

    temp_converter_2

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,16 @@
 
+//when the user clicks the #fahrenheit_to_celsius
+	//Get the value from #tempurature and store in variable fahrenheit
+	//Convert that value to a number
+
+	//Convert to celsius and store in variable celsius
+		//((fahrenheit - 32) / 1.8)
+
+	//Display in #result (hint: use HTML method)
+
+
+$('#fahrenheit_to_celsius').on('click', function(){
+	var fahrenheit = $('#tempurature').val();
+	fahrenheit = parseFloat(fahrenheit);
+	var celsius = $('')
+});
