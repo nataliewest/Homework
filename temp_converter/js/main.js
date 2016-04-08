f7e974799f8063c96c78cc64b971c3247af4b6cd
nataliewest <natalie.west@serenaandlily.com>
2016-04-07 21:21:22 -0700

    temp_converter_take_2

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,4 +1,6 @@
 
+//Converting Fahrenheit to Celsius
+
 //when the user clicks the #fahrenheit_to_celsius
 	//Get the value from #tempurature and store in variable fahrenheit
 	//Convert that value to a number
@@ -6,13 +8,46 @@
 	//Convert to celsius and store in variable celsius
 		//((fahrenheit - 32) / 1.8)
 
+		//convert celsius to fahrenheit
+		//1.8 * celsius + 32;
+
 	//Display in #result (hint: use HTML method)
 
 
 $('#fahrenheit_to_celsius').on('click', function(){
-	var fahrenheit = $('#tempurature').val();
+	var fahrenheit = $('#temperature').val();
 	fahrenheit = parseFloat(fahrenheit);
-	var celsius = $('')
+	console.log(fahrenheit)
+	var celsius = (fahrenheit - 32) / 1.8;
+	$('#result').html(celsius);
 });
 
 
+//Converting Celsius to Fahrenheit
+
+//when the user clicks the #celsius_to_fahrenheit
+	//Get the value from #tempurature and store in variable celsius
+	//Convert that value to a number
+
+	//Convert to fahrenheit and store in variable celsius
+		
+		//1.8 * celsius + 32;
+
+	//Display in #result (hint: use HTML method)
+
+	$('#celsius_to_fahrenheit').on('click', function(){
+		var celsius = $('#temperature').val();
+		celsius = parseFloat(celsius);
+		var fahrenheit = 1.8 * celsius + 32;
+		$('#result').html(fahrenheit);
+	});
+
+
+
+
+
+
+
+
+
+	
