361b458156c75b7559f9d2a5c4a730d869e098dc
nataliewest <natalie.west@serenaandlily.com>
2016-04-14 19:53:13 -0700

    carousal_take_three

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -13,20 +13,34 @@ $('#next').on('click', function(){
 	console.log('clicked')
 	currentPosition = currentPosition + 1;
 	//images[currentPosition];
+
+	if(currentPosition === 5){
+		$('#next').prop('disabled', true);
+
+//if it's the last image, the next button will be disabled
+
+	}
+	console.log('images', images[currentPosition])
+	$('#image-to-vote-on').attr('src', 'images/' + images[currentPosition]);
+	$('#prev').prop('disabled', false);
+
+});
+
+	//Add one to the current position	
+	
+$('#prev').on('click', function(){
+	console.log('clicked')
+	currentPosition = currentPosition - 1;
+	//images[currentPosition];
 	console.log('images', images[currentPosition])
 	$('#image-to-vote-on').attr('src', 'images/' + images[currentPosition]);
 
-});
-
-
-	//Add one to the current position
-
-$('previous').on('click', function(){
-	currentPosition = currentPosition + 1;
-	$('#image-to-vote-on').attr('src', 'images/' + images[currentPosition]);
-
+	if(currentPosition === 0){
+		$('#prev').prop('disabled', true);
+	}
 });
 	
+var votes = [0,0,0,0,0,0];	
 
 	// var firstImages = images [0]; 
 
